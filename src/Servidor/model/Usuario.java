59d7b4d05ea1458a385e658841fee237de95992f
DanCmo <dankurso845@gmail.com>
2024-04-24 21:11:33 -0500

    Carga hipoteticamente completa 2

--- src/Servidor/model/Usuario.java
+++ src/Servidor/model/Usuario.java
@@ -6,11 +6,9 @@ import java.util.ArrayList;
 public class Usuario {
     private String nombre;
 
-    private ArrayList<Historial> historiales;
 
     public Usuario(String nombre) {
         this.nombre = nombre;
-        historiales = new ArrayList<Historial>();
     }
 
     public Usuario() {
@@ -25,11 +23,6 @@ public class Usuario {
         this.nombre = nombre;
     }
 
-    public ArrayList<Historial> getHistoriales() {
-        return historiales;
-    }
 
-    public void setHistoriales(ArrayList<Historial> historiales) {
-        this.historiales = historiales;
-    }
+
 }
